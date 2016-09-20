8f4cd27a75499dab206b4e3e67065c4a4e0ca0dc
Wong Shao Voon <shaovoon@yahoo.com>
2016-09-20 19:52:38 +0800

    Convert enum to description

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Python Script to Prettify FIX Protocol Message Logs with Information
 **Note:** 4 parameters, see below.
 
 ```python
-python prettify_fix_log.py <path to FIXT11.xml> <path to FIX50SP2.xml> <src log file> <output prettified log file>
+python prettify_fix_log.py [-h][-e] <src log file> <output prettified log file> <path to FIX50SP2.xml> <optional path to FIXT11.xml>
+Option -h: display usage
+Option -e: convert enum to description
 ```
--- src/prettify_fix_log.py
+++ src/prettify_fix_log.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
+
 # Python Script to Prettify FIX Protocol Message Logs with Information
 # ====================================================================
 # Author: Wong Shao Voon
+# Version 1.0.0 
 # Copyright 2016
-# No warranty implied
-
-#!/usr/bin/python
+# No warranty implied or expressed
 
 import string
 import sys
@@ -12,18 +13,25 @@ import getopt
 import datetime
 import xml.etree.ElementTree as ET
 
-def parse_xml(file, dict, msg_dict):
+def parse_xml(file, dict, msg_dict, field_enum_dict):
     print "Parsing file: %s" % file
     tree = ET.parse(file)
     root = tree.getroot()
     fields = root.findall("fields")[0].findall('field')
     for field in fields:
         dict[int(field.attrib['number'])] = field.attrib['name']
+        values = field.findall("value")
+        enum_dict = {}
+        for value in values:
+            enum_dict[value.attrib['enum']] = value.attrib['description']
+        if len(enum_dict) > 0:
+            field_enum_dict[int(field.attrib['number'])] = enum_dict
+        
     msgs = root.findall("messages")[0].findall('message')
     for msg in msgs:
         msg_dict[msg.attrib['msgtype']] = msg.attrib['name']
 
-def prettify(src_file, dest_file, dict, msg_dict):
+def prettify(src_file, dest_file, dict, msg_dict, field_enum_dict, parse_enum):
     with open(dest_file, 'wb') as fo:
         with open(src_file, 'rb') as f:
             line_num = 0
@@ -32,6 +40,8 @@ def prettify(src_file, dest_file, dict, msg_dict):
                 s = f.readline()
                 if len(s) == 0: break
                 columns = s.split(chr(1))
+                if len(columns) == 1:
+                    columns = s.split('|')
                 for column in columns:
                     values = column.split('=')
                     if len(values) > 1:
@@ -45,6 +55,13 @@ def prettify(src_file, dest_file, dict, msg_dict):
                                 except KeyError:
                                     print("msg_name KeyError: %d, value: %s, line_num: %d: tag 35 do not have %s msg listed" % (key, values[1], line_num, values[1]))
                                     fo.write("%s(%d)=%s|" % (tag_name, key, values[1]))
+                            elif parse_enum and field_enum_dict.has_key(key):
+                                enum_dict = field_enum_dict[key]
+                                if enum_dict.has_key(values[1]):
+                                    fo.write("%s(%d)=%s(%s)|" % (tag_name, key, enum_dict[values[1]], values[1]))
+                                else:
+                                    fo.write("%s(%d)=%s|" % (tag_name, key, values[1]))
+                                    print("enum KeyError: %d, value: %s, line_num: %d: tag %d does not have this enum value in dictionary: %s" % (key, values[1], line_num, key, values[1]))
                             else:
                                 fo.write("%s(%d)=%s|" % (tag_name, key, values[1]))
                         except ValueError:
@@ -55,38 +72,48 @@ def prettify(src_file, dest_file, dict, msg_dict):
                 fo.write("\n")
 
 def usage():
-    print "Usage: prettify_fix_log.py [-h] <path to FIXT11.xml> <path to FIX50SP2.xml> <src log file> <output prettified log file>"
+    print "Usage: prettify_fix_log.py [-h][-e] <src log file> <output prettified log file> <path to FIX50SP2.xml> <optional path to FIXT11.xml>"
     print "Option -h: display usage"
+    print "Option -e: convert enum to description"
 
-# python prettify_fix_log.py FIXT11.xml FIX50SP2.xml log.txt log2.txt
+# python prettify_fix_log.py log.txt log2.txt FIX50SP2.xml FIXT11.xml
 
 if __name__ == "__main__":
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", [])
+        opts, args = getopt.getopt(sys.argv[1:], "he", [])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
-        
+    
+    parse_enum = False
     for option in opts:
         if option[0] == '-h':
             usage()
             sys.exit(2)
+        if option[0] == '-e':
+            parse_enum = True
+            
+    print("parse_enum:%s" % parse_enum)
 
-    if len(args) < 4:
+    if len(args) < 3:
         print "Not enough arguments.\n"
         usage()
         sys.exit(2)
 
-dict = {}
-msg_dict = {}
+    dict = {}
+    msg_dict = {}
+    field_enum_dict = {}
 
-xml_file1 = args[0]
-xml_file2 = args[1]
-src_log = args[2]
-output_log = args[3]
+    src_log = args[0]
+    output_log = args[1]
 
-parse_xml(xml_file1, dict, msg_dict)
-parse_xml(xml_file2, dict, msg_dict)
-prettify(src_log, output_log, dict, msg_dict)
+    xml_file1 = args[2]
+    parse_xml(xml_file1, dict, msg_dict, field_enum_dict)
+
+    if len(args) == 4:
+        xml_file2 = args[3]
+        parse_xml(xml_file2, dict, msg_dict, field_enum_dict)
+        
+    prettify(src_log, output_log, dict, msg_dict, field_enum_dict, parse_enum)
